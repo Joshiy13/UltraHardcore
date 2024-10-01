774cd5141424cc17d34d6a605f32880aa3324c89
Josh <joshua@weidanz.de>
2024-10-01 10:44:12 +0200

    - Made Fire burn until extinguished - Set difficulty to Hard - Turned off Natural Regeneration

---
+++ src/client/java/dev/joshiy13/ultrahardcore/client/UltrahardcoreClient.java
@@ -0,0 +1,10 @@
+package dev.joshiy13.ultrahardcore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltrahardcoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/joshiy13/ultrahardcore/Ultrahardcore.java
@@ -0,0 +1,29 @@
+package dev.joshiy13.ultrahardcore;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.Difficulty;
+
+public class Ultrahardcore implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ServerTickEvents.START_SERVER_TICK.register(this::onServerTick);
+        ServerTickEvents.END_SERVER_TICK.register(this::onServerTickEnd);
+    }
+
+    private void onServerTick(MinecraftServer server) {
+        server.getGameRules().get(GameRules.NATURAL_REGENERATION).set(false, server);
+        server.setDifficulty(Difficulty.HARD, true);
+    }
+
+    private void onServerTickEnd(MinecraftServer server) {
+        server.getPlayerManager().getPlayerList().forEach(player -> {
+            if (player.isOnFire() && !player.hasStatusEffect(StatusEffects.FIRE_RESISTANCE)) {
+                player.setFireTicks(20);
+            }
+        });
+    }
+}
